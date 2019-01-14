f0574c626bbaf5acfc3cfadee347950ba4c4263b
Samwise0000 <ben@dream-front.com>
2019-01-13 17:32:21 -0700

    End of 321

--- Source/MissionSurvive/MissionSurvive.Build.cs
+++ Source/MissionSurvive/MissionSurvive.Build.cs
@@ -8,6 +8,6 @@ public class MissionSurvive : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "AIModule", "GameplayTasks" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "AIModule", "NavigationSystem", "GameplayTasks" });
 	}
 }
